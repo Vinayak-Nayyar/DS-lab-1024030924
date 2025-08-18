dfe27073c7703a3dc7aaa8bc9ee8fc0f26a1d525
Vinayak-Nayyar <vinayaknayyar30@gmail.com>
2025-08-18 23:07:21 +0530

    Update ques4(a).cpp

--- Assignment-2/ques4(a).cpp
+++ Assignment-2/ques4(a).cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1 = "Hello ";
+    string str2 = "World";
+
+    string result = str1 + str2; // concatenation
+    cout << "Concatenated string: " << result << endl;
+
+    return 0;
+}
 
